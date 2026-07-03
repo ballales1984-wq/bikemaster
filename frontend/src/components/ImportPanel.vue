3861e21b6677945b043204503514d026f262c5b2
alessio <ballales1984@gmail.com>
2026-07-03 17:40:50 +0200

    fix: disable Google Health button until OAuth is configured

--- frontend/src/components/ImportPanel.vue
+++ frontend/src/components/ImportPanel.vue
@@ -42,14 +42,14 @@
         <span>or</span>
       </div>
 
-      <button @click="connectGoogleHealth" class="btn btn-google-fit" :disabled="importing" type="button">
+      <button @click="connectGoogleHealth" class="btn btn-google-fit" :disabled="true" type="button" title="Not configured - coming soon">
         <svg viewBox="0 0 24 24" width="18" height="18" style="margin-right: 6px;">
           <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.76h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"/>
           <path fill="#34A853" d="M12 23c3.05 0 5.84-1.15 7.86-3l-3.57-2.76c-.98.66-2.23 1.06-3.62 1.44v2.26C15.24 21.23 13.71 22 12 22z"/>
           <path fill="#FBBC05" d="M6.27 15.73a7.5 7.5 0 0 1 0-3.46l2.93-2.27a7.5 7.5 0 0 0 1.74 3.19l-2.93 2.27z"/>
           <path fill="#EA4335" d="M18.57 6.43a7.5 7.5 0 0 0-6.57-4.43 7.5 7.5 0 0 0-1.57.23l2.93 2.26a4.99 4.99 0 0 1 5.17 4.17z"/>
         </svg>
-        {{ importing ? 'Connecting...' : 'Import from Google Health' }}
+        Coming soon
       </button>
 
       <div id="import-progress" v-if="status" class="result-box">{{ status }}</div>
